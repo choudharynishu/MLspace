c7c2d95a7b784f62e304a2dfc1b6887bd38151e5
Nishu Choudhary <nishuchoudhary@gatech.edu>
2023-12-02 15:07:59 -0600

    Changed python notebook to python script

--- DataExtraction.py
+++ DataExtraction.py
@@ -18,69 +18,28 @@ import rosbag
 import bagpy
 from bagpy import bagreader
 
-
-# In[2]:
-
-
 #Create roles to give learning and histing access to data
 role = get_execution_role()
 region = boto3.Session().region_name
 sess = sagemaker.Session()
 
 
-# In[3]:
-
-
 # S3 bucket where the original data is downloaded and stored.
 downloaded_data_bucket = f"mlspace-data-981080730561"
 downloaded_data_prefix = "global/datasets/TRBChallengeData"
 
+#Load the dataset
+s3 = boto3.client('s3')
+s3.download_file(downloaded_data_bucket,f"{downloaded_data_prefix}/2023-11-03-12-37-20.bag","2023-11-03-12-37-20.bag")
 
-# In[4]:
-
-
-get_ipython().run_cell_magic('time', '', '\n# Load the dataset\ns3 = boto3.client("s3")\ns3.download_file(downloaded_data_bucket, f"{downloaded_data_prefix}/2023-11-03-12-37-20.bag", "2023-11-03-12-37-20.bag")\n\nwith rosbag.Bag(\'2023-11-03-12-37-20.bag\', \'r\') as bag:\n    # Iterate through the messages in the bag\n    for topic, msg, t in bag.read_messages():\n        # Process or print information about the message\n        print(f"Topic: {topic}, Timestamp: {t.to_sec()}")\n        # ... do something with the message content (msg)\n')
-
-
-# In[ ]:
-
-
-s3 = s3fs.S3FileSystem()
-s3_address = 's3://mlspace-data-981080730561/global/datasets/TRBChallengeData/2023-11-03-12-37-20.bag'
-
-chunk_size=786432
-
-bag = bagreader(s3.open(s3_address))
-# Now you can iterate through the messages in the bag
-
-for topic, msg, t in bag.read_messages():
-    # Process each message as needed
-    print(f"Topic: {topic}, Timestamp: {t}, Message: {msg}")
-
-# Close the bag file
-bag.close()
-
-
-# In[ ]:
-
-
-counter=0
-# Open the bag file
 with rosbag.Bag('2023-11-03-12-37-20.bag', 'r') as bag:
     # Iterate through the messages in the bag
     for topic, msg, t in bag.read_messages():
-        counter+=1
         # Process or print information about the message
-        if counter>=10:
             print(f"Topic: {topic}, Message: {msg}, Timestamp: {t.to_sec()}")
-            print('*******************************************')
-        elif counter>=15:
-            break
         # ... do something with the message content (msg)
 bag.close()
 
-
-# In[ ]:
 
 
 
